ff7453544e5e6a47b0d631f84f355d7fde8a1ebc
Pyrolee <a1263394777@gmail.com>
2024-07-24 15:13:12 +0800

    Site updated: 2024-07-24 15:13:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/24/hello-world/","2024/07/24/Intro/"];function toRandomPost(){
+var posts=["2024/07/24/Intro/","2024/07/24/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
